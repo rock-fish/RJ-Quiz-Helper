8e47b8635e6dc239f59b8f76438de847cc56fac6
rock-fish <93435100+rock-fish@users.noreply.github.com>
2021-11-16 21:11:04 -0500

    Update verses.js
    Added James 3

--- verses.js
+++ verses.js
@@ -103,30 +103,29 @@ export const verses = {
             "25": "In the same way, 1=was not even Rahab the prostitute considered 1|righteous 2=for what she did when she gave 2|lodging to the spies and sent them off in a different direction?",
             "26": "1=As the body without 1|the spirit is dead, so faith without deeds is dead."
           }
-        }
-        /*  ADD QUESTIONS
+        },
         "3": {
           verse: {
             "1": "1=Not many of you should become 1|teachers, my fellow believers, because 2=you know 2|that we who teach 3=will be judged 3|more strictly.",
-            "2": "We 2=all stumble 2|in many ways. Anyone who is 2=never at 2|fault in what they say is perfect, able 3=to keep their 3|whole body in check.",
+            "2": "We 1=all stumble 1|in many ways. Anyone who is 2=never at 2|fault in what they say is perfect, able 3=to keep their 3|whole body in check.",
             "3": "When 1=we put 1|bits into the mouths of horses 2=to make them 2|obey us, 3=we can turn 3|the whole animal.",
             "4": "1=Or take 1|ships as an example. 2=Although 2|they are so large and are driven by strong winds, 3=they are steered 3|by a very small rudder 4=wherever 4|the pilot wants to go.",
             "5": "1=Likewise, 1|the tongue is a small part of the body, but 2=it makes 2|great boasts. Consider 3=what a great 3|forest is 4=set on fire 4|by a small spark.",
-            "6": "1=The tongue also 1|is a fire, 2=a world of 2|evil among the parts of the body. 3=It 3|corrupts the whole body, sets the whole course of one’s life on fire, and is itself set on fire by hell.",
+            "6": "1=The tongue also 1|is a fire, 2=a world of 2|evil among the parts of the body. 3=It corrupts 3|the whole body, sets the whole course of one’s life on fire, and is itself set on fire by hell.",
             "7": "1=All kinds of 1|animals, birds, reptiles and sea creatures are being tamed and have been tamed by mankind,",
             "8": "but 2-no human 2|being can tame the tongue. 3=It is 3|a restless evil, full of deadly poison.",
             "9": "1=With the tongue we 1|praise our Lord and Father, and 2=with it we 2|curse human beings, who have been made in God’s likeness.",
             "10": "1=Out of the same 1|mouth come praise and cursing. 2=My brothers and sisters, 2|this should not be.",
-            "11": "Can both fresh water and salt water flow from the same spring?",
-            "12": "My brothers and sisters, can a fig tree bear olives, or a grapevine bear figs? Neither can a salt spring produce fresh water.",
-            "13": "Who is wise and understanding among you? Let them show it by their good life, by deeds done in the humility that comes from wisdom.",
-            "14": "But if you harbor bitter envy and selfish ambition in your hearts, do not boast about it or deny the truth.",
-            "15": "Such \"wisdom\" does not come down from heaven but is earthly, unspiritual, demonic.",
-            "16": "For where you have envy and selfish ambition, there you find disorder and every evil practice.",
-            "17": "But the wisdom that comes from heaven is first of all pure; then peace-loving, considerate, submissive, full of mercy and good fruit, impartial and sincere.",
-            "18": "Peacemakers who sow in peace reap a harvest of righteousness."
+            "11": "1=Can both 1|fresh water and salt water 2=flow from 2|the same spring?",
+            "12": "My brothers and sisters, 1=can a 1|fig tree bear olives, or a 2=grapevine bear 2|figs? 3=Neither can 3|a salt spring produce fresh water.",
+            "13": "1=Who is wise 1|and understanding among you? 2=Let them show it by 2|their good life, 3=by deeds done in 3|the humility that comes from wisdom.",
+            "14": "But 1=if you harbor 1|bitter envy and selfish ambition in your hearts, 1=do not boast 1|about it or deny the truth.",
+            "15": "Such \"wisdom\" 1=does not come down from 1|heaven but is earthly, unspiritual, demonic.",
+            "16": "For 1=where you have 1|envy and selfish ambition, 2=there you find 2|disorder and every evil practice.",
+            "17": "But the wisdom that comes from heaven is 1=first of all 1|pure; then peace-loving, considerate, submissive, 2=full of 2|mercy and good fruit, impartial and sincere.",
+            "18": "Peacemakers 1=who sow in 1|peace reap a harvest of righteousness."
           }
-        }*/
+        }
       }
     }
   }
