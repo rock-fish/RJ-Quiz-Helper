c6b062c8478a64e1d9fe5c6389812832d32a7d58
rock-fish <93435100+rock-fish@users.noreply.github.com>
2021-11-17 21:17:24 -0500

    Update verses.js
    added James 5

--- verses.js
+++ verses.js
@@ -1,10 +1,11 @@
+//Replace ’ '      “” \"    — -
+// 1= beginning of question          1| end of question
 export const verses = {
   book: {
     "romans": {
       chapter: {
         "1": {
           verse: { 
-            // | = end of question | = = beginning of question
             "1": "1=Paul, a servant of 1| Christ Jesus, 2=called to be 2|an apostle and 3=set apart for 3|the gospel of God",
             "2": "1=the gospel 2=he 1| promised beforehand through his 2|prophets in the Holy Scriptures",
             "3": "regarding his Son, 1=who as to 1| his earthly life was a descendant of David,",
@@ -78,7 +79,7 @@ export const verses = {
           verse: {
             "1": "1=My brothers and sisters, believers 1|in our glorious 2=Lord Jesus Christ must 2|not show favoritism.",
             "2": "1=Suppose a man comes 1|into 2=your meeting wearing 2|a gold ring and fine clothes, and a poor man in filthy old clothes also comes in.",
-            "3": "1=If you show special attention to 1|the man wearing fine clothes and say, \"Here’s a good seat for you,\" 2=but say to 2|the poor man, \"You stand there\" or \"Sit on the floor by my feet,\"",
+            "3": "1=If you show special attention to 1|the man wearing fine clothes and say, \"Here's a good seat for you,\" 2=but say to 2|the poor man, \"You stand there\" or \"Sit on the floor by my feet,\"",
             "4": "1=have you not 1|discriminated among yourselves and become judges with evil thoughts?",
             "5": "Listen, my dear brothers and sisters: 1=Has not God chosen 1|those who are poor 1=in the eyes of the world to be 1|rich in faith and to inherit the kingdom he promised those who love him?",
             "6": "1=But you have dishonored 1|the poor. 2=Is it not the rich 2|who are exploiting you? 3=Are they not the ones who are 3|dragging you into court?",
@@ -98,7 +99,7 @@ export const verses = {
             "20": "You foolish person, 1=do you want evidence that 1|faith without deeds is useless?",
             "21": "Was not 1=our father Abraham considered 1|righteous for what he did 2=when he offered his 2|son Isaac on the altar?",
             "22": "You see that 1=his faith and his 1|actions were working together, and 2=his faith was made 2|complete by what he did.",
-            "23": "And 1=the scripture was fulfilled that says, 1|\"Abraham believed God, and 2=it was credited to him as 2|righteousness,\" and he was called God’s friend.",
+            "23": "And 1=the scripture was fulfilled that says, 1|\"Abraham believed God, and 2=it was credited to him as 2|righteousness,\" and he was called God's friend.",
             "24": "You see that 1=a person is considered righteous 1|by what they do and not by faith alone.",
             "25": "In the same way, 1=was not even Rahab the prostitute considered 1|righteous 2=for what she did when she gave 2|lodging to the spies and sent them off in a different direction?",
             "26": "1=As the body without 1|the spirit is dead, so faith without deeds is dead."
@@ -111,10 +112,10 @@ export const verses = {
             "3": "When 1=we put 1|bits into the mouths of horses 2=to make them 2|obey us, 3=we can turn 3|the whole animal.",
             "4": "1=Or take 1|ships as an example. 2=Although 2|they are so large and are driven by strong winds, 3=they are steered 3|by a very small rudder 4=wherever 4|the pilot wants to go.",
             "5": "1=Likewise, 1|the tongue is a small part of the body, but 2=it makes 2|great boasts. Consider 3=what a great 3|forest is 4=set on fire 4|by a small spark.",
-            "6": "1=The tongue also 1|is a fire, 2=a world of 2|evil among the parts of the body. 3=It corrupts 3|the whole body, sets the whole course of one’s life on fire, and is itself set on fire by hell.",
+            "6": "1=The tongue also 1|is a fire, 2=a world of 2|evil among the parts of the body. 3=It corrupts 3|the whole body, sets the whole course of one's life on fire, and is itself set on fire by hell.",
             "7": "1=All kinds of 1|animals, birds, reptiles and sea creatures are being tamed and have been tamed by mankind,",
             "8": "but 2-no human 2|being can tame the tongue. 3=It is 3|a restless evil, full of deadly poison.",
-            "9": "1=With the tongue we 1|praise our Lord and Father, and 2=with it we 2|curse human beings, who have been made in God’s likeness.",
+            "9": "1=With the tongue we 1|praise our Lord and Father, and 2=with it we 2|curse human beings, who have been made in God's likeness.",
             "10": "1=Out of the same 1|mouth come praise and cursing. 2=My brothers and sisters, 2|this should not be.",
             "11": "1=Can both 1|fresh water and salt water 2=flow from 2|the same spring?",
             "12": "My brothers and sisters, 1=can a 1|fig tree bear olives, or a 2=grapevine bear 2|figs? 3=Neither can 3|a salt spring produce fresh water.",
@@ -124,6 +125,51 @@ export const verses = {
             "16": "For 1=where you have 1|envy and selfish ambition, 2=there you find 2|disorder and every evil practice.",
             "17": "But the wisdom that comes from heaven is 1=first of all 1|pure; then peace-loving, considerate, submissive, 2=full of 2|mercy and good fruit, impartial and sincere.",
             "18": "Peacemakers 1=who sow in 1|peace reap a harvest of righteousness."
+          }
+        },
+        "4": {
+          verse: {
+            "1": "What 1=causes fights 1|and quarrels among you? Don't 2=they come 2|from your 3=desires that 3|battle within you?",
+            "2": "1=You desire 1|but do not have, so you kill. You covet but you 2=cannot get 2|what you want, so you quarrel and fight. You do not have because 3=you do not ask 3|God.",
+            "3": "When you ask, 1=you do not receive, 1|because you ask with wrong motives, that 2=you may spend 2|what you get on your pleasures.",
+            "4": "1=You adulterous people, 1|don't you know that 2=friendship with 2|the world means enmity against God? Therefore, 3=anyone who chooses 3|to be a friend of the world becomes an enemy of God.",
+            "5": "Or do you think 1=Scripture says 1|without reason that he jealously 2=longs for 2|the spirit he has caused to dwell in us?",
+            "6": "But 1=he gives 1|us more grace. That is why 2=Scripture says: 2|\"God opposes the proud but shows favor to the humble.\"",
+            "7": "1=Submit 1|yourselves, then, to God. 2=Resist 2|the devil, and he will flee from you.",
+            "8": "1=Come near 1|to God and he will come near to you. Wash 2=your hands, 2|you sinners, and purify your hearts, you double-minded.",
+            "9": "1=Grieve, 1|mourn and wail. 2=Change your 2|laughter to mourning and your joy to gloom.",
+            "10": "Humble yourselves before the Lord, and he will lift you up.",
+            "11": "Brothers and sisters, 1=do not slander 1|one another. 2=Anyone who speaks 2|against a brother or sister or judges them speaks against the law and judges it. 3=When you judge 3|the law, you are not keeping it, but sitting in judgment on it.",
+            "12": "1=There is only one 1|Lawgiver and Judge, the one 2=who is able to 2|save and destroy. 3=But you - 3|who are you to judge your neighbor?",
+            "13": "Now listen, 1=you who say, 1|\"Today or tomorrow we will go to this or that city, spend a year there, carry on business and make money.\"",
+            "14": "Why, 1=you do not even know 1|what will happen tomorrow. What is your life? 2=You are a 2|mist that appears for a little while and then vanishes.",
+            "15": "Instead, 1=you ought to say, 1|\"If it is the Lord's will, we will live and do this or that.\"",
+            "16": "As it is, 1=you boast in 1|your arrogant schemes. All such boasting is evil.",
+            "17": "1=If anyone, then, 1|2=knows the 2|good 3=they ought to 3|do and doesn't do it, it is sin for them."
+          }
+        },
+        "5": {
+          verse: {
+            "1": "1=Now listen, 1|you rich people, 2=weep and 2|wail because of the misery that is coming on you.",
+            "2": "1=Your wealth 1|has rotted, and moths have eaten your clothes.",
+            "3": "1=Your gold and 1|silver are corroded. Their corrosion 2=will testify 2|against you and eat your flesh like fire. 1=You have hoarded 1|wealth in the last days.",
+            "4": "Look! 1=The wages you 1|failed to pay the workers who mowed your fields are crying out against you. 2=The cries of 2|the harvesters have reached the ears of the Lord Almighty.",
+            "5": "1=You have lived 1|on earth in luxury and self-indulgence. 2=You have fattened 2|yourselves in the day of slaughter.",
+            "6": "1=You have condemned 1|and murdered the innocent one, who was not opposing you.",
+            "7": "1=Be patient, 1|then, brothers and sisters, 2=until 2|the Lord's coming. 3=See how 3|the farmer waits for the land to 4=yield its 4|valuable crop, patiently 5=waiting for 5|the autumn and spring rains.",
+            "8": "You too, 1=be patient and 1|stand firm, because the 2=Lord's coming 2|is near.",
+            "9": "1=Don't grumble 1|against one another, brothers and sisters, or 2=you will be 2|judged. 3=The Judge 3|is standing at the door!",
+            "10": "Brothers and sisters, 1=as an example of 1|patience in the face of suffering, 2=take the 2|prophets who spoke in the name of the Lord.",
+            "11": "As you know, 1=we count as 1|blessed those who have persevered. 2=You have heard of 2|Job's perseverance and have seen what the Lord finally brought about. 3=The Lord is 3|full of compassion and mercy.",
+            "12": "Above all, 1=my brothers and sisters, do not 1|swear-not by heaven or by earth or by anything else. All you need to say is a simple \"Yes\" or \"No.\" Otherwise you will be condemned.",
+            "13": "1=Is anyone among you in 1|trouble? Let them pray. 2=Is anyone 2|happy? Let them sing songs of praise.",
+            "14": "1=Is anyone among you 1|sick? Let them call the elders 2=of the church to 2|pray over them and anoint them with oil in the name of the Lord.",
+            "15": "And 1=the prayer offered in 1|faith will make the sick person well; the Lord will raise them up. 2=If they have 2|sinned, they will be forgiven.",
+            "16": "Therefore 1=confess your sins to 1|each other and pray for each other so that you may be healed. The 2=prayer of a 2|righteous person is powerful and effective.",
+            "17": "Elijah was a human being, even as we are. 1=He prayed earnestly 1|that it would not rain, and it did not rain on the land for three and a half years.",
+            "18": "Again he prayed, and 1=the heavens gave 1|rain, and the earth produced its crops.",
+            "19": "My brothers and sisters, 1=if one of you should 1|wander from the truth and someone should bring that person back,",
+            "20": "remember this: 1=Whoever turns a 1|sinner from the error of their way 2=will save them from 2|death and cover over a multitude of sins."
           }
         }
       }
